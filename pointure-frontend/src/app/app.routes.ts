82f78e000367da6fdffd3f0f34f5e5c6006d4f69
Diego NL <diegonoviluce28@gmail.com>
2025-06-09 00:50:40 +0200

    More changes

--- pointure-frontend/src/app/app.routes.ts
+++ pointure-frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [{ path: '', component: HomeComponent }],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
